13dada9c29c14291f54924a95db95125d8956d24
codymarcus <codydavidmarcus@gmail.com>
2015-02-01 15:06:29 -0500

    Added respawn if you fall

---
+++ CSC404 Test/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public GameObject spawn;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "Death")
+		{
+			transform.position = spawn.transform.position;
+		}
+	}
+}
