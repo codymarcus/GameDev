c2dac1287aa8749eb075528fff78c71a97b9fe30
Bardia Khorshid-Doust <bardiad@bdmbp.acs.ocad.ca>
2015-02-04 17:28:47 -0500

    reorganized game objects and assets

---
+++ CSC404 Test/Assets/Scripts/Character Scripts/Aim.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Aim : MonoBehaviour {
+
+	public int playerNumber;
+	Vector2 rightStick = new Vector2(0, 0);
+	float angularVelocity = 30f;
+	float radialDeadZone = 0.25f;
+	Vector3 direction;
+	Quaternion currentRotation;
+
+	// Use this for initialization
+	void Start () {
+		 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rightStick = new Vector2 (Input.GetAxis (playerNumber + "RightH"), Input.GetAxis (playerNumber + "RightV"));
+		direction = new Vector3 (rightStick.x, -1 * rightStick.y, 0);
+		if (direction.magnitude > radialDeadZone)
+		{
+			currentRotation = Quaternion.LookRotation(Vector3.forward, direction);
+			transform.rotation = Quaternion.Lerp(transform.rotation, currentRotation, Time.deltaTime * angularVelocity);
+		}
+
+	}
+}
---
+++ CSC404 Test/Assets/Scripts/Character Scripts/Weapon.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public int playerNumber;
+	public GameObject bullet;
+	public GameObject barrel;
+	public PlayerController player;
+	float canFire = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetButton(playerNumber + "Fire1") && canFire <= 0 && player.UseAmmo()) 
+		{ 
+			GameObject b = new GameObject();
+			b = Instantiate(bullet, barrel.transform.position, barrel.transform.rotation) as GameObject;
+			b.GetComponent<Bullet>().owner = playerNumber;
+			Destroy( b , 4);
+			canFire = .2f;
+		}
+		if (canFire <= 0)
+			canFire = 0;
+		else
+			canFire -= Time.deltaTime;
+	}
+}
