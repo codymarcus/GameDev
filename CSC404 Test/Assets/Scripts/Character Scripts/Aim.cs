260b9ea67c8c7f5edb64da3fa3e4cb5083777378
kyle1991 <caojiaming256@gmail.com>
2015-02-06 17:35:46 -0500

    reverse back the aim controller

--- CSC404 Test/Assets/Scripts/Character Scripts/Aim.cs
+++ CSC404 Test/Assets/Scripts/Character Scripts/Aim.cs
@@ -9,7 +9,7 @@ public class Aim : MonoBehaviour {
 	float radialDeadZone = 0.25f;
 	Vector3 direction;
 	Quaternion currentRotation;
-	public int RotateSpeed = 300;
+	//public int RotateSpeed = 300;
 
 	// Use this for initialization
 	void Start () {
@@ -18,14 +18,14 @@ public class Aim : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Rotate (-(Vector3.forward * RotateSpeed * Time.deltaTime * Input.GetAxis(playerNumber + "RightH")));
-		//rightStick = new Vector2 (Input.GetAxis (playerNumber + "RightH"), Input.GetAxis (playerNumber + "RightV"));
-		//direction = new Vector3 (rightStick.x, -1 * rightStick.y, 0);
-		//if (direction.magnitude > radialDeadZone)
-		//{
-		//	currentRotation = Quaternion.LookRotation(Vector3.forward, direction);
-		//	transform.rotation = Quaternion.Lerp(transform.rotation, currentRotation, Time.deltaTime * angularVelocity);
-		//}
+		//transform.Rotate (-(Vector3.forward * RotateSpeed * Time.deltaTime * Input.GetAxis(playerNumber + "RightH")));
+		rightStick = new Vector2 (Input.GetAxis (playerNumber + "RightH"), Input.GetAxis (playerNumber + "RightV"));
+		direction = new Vector3 (rightStick.x, -1 * rightStick.y, 0);
+		if (direction.magnitude > radialDeadZone)
+		{
+			currentRotation = Quaternion.LookRotation(Vector3.forward, direction);
+			transform.rotation = Quaternion.Lerp(transform.rotation, currentRotation, Time.deltaTime * angularVelocity);
+		}
 
 	}
 }
