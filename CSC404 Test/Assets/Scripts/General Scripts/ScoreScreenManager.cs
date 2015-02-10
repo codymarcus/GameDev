3d3dbaed7939681d8ea9856117ab78cf044d5632
codymarcus <codydavidmarcus@gmail.com>
2015-02-10 12:40:20 -0500

    Score screen transitions to round screen

--- CSC404 Test/Assets/Scripts/General Scripts/ScoreScreenManager.cs
+++ CSC404 Test/Assets/Scripts/General Scripts/ScoreScreenManager.cs
@@ -5,17 +5,20 @@ using System.Collections;
 public class ScoreScreenManager : MonoBehaviour {
 
 	public static int[] matchScores = {0, 0, 0, 0};
+	public float waitTime;
 	string displayText;
 
 	// Use this for initialization
 	void Start () {
-	
+		displayText = "Player 1: " + matchScores [0] + "\nPlayer 2: " + matchScores [1] + "\nPlayer 3: "
+			+ matchScores [2] + "\nPlayer 4: " + matchScores [3];
+		GameObject.FindGameObjectWithTag ("Scores").GetComponent<Text> ().text = displayText;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		displayText = "Player 1: " + matchScores [0] + "\nPlayer 2: " + matchScores [1] + "\nPlayer 3: "
-						+ matchScores [2] + "\nPlayer 4: " + matchScores [3];
-		GameObject.FindGameObjectWithTag ("Scores").GetComponent<Text> ().text = displayText;
+		waitTime -= Time.deltaTime;
+		if (waitTime <= 0)
+			Application.LoadLevel (0);
 	}
 }
