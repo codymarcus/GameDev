2f360d2360b66835d04da2736eff59d641bc1d46
Sida Chen <sidachen@inside-196-20.wireless.utoronto.ca>
2015-03-04 14:22:04 -0500

    Add GameMenu to the game which allows one to select game mode.

--- CSC404/Assets/Scripts/General Scripts/MatchManager.cs
+++ CSC404/Assets/Scripts/General Scripts/MatchManager.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 public class MatchManager : MonoBehaviour {
 
 	public static int roundNumber = 1;
-	public string[] gameTypes = {"Last Man Standing", "Last Team Standing", "King of the Hill", "WANTED"};
+	public string[] gameTypes = {"Deathmatch", "Team Deathmatch", "King of the Hill", "Team KOTH"};
 	public static string gameType;
 	public string[] possibleTeamDynamics = {"FFA", "2v2"};
 	public static string teamDynamic;
@@ -22,6 +22,8 @@ public class MatchManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		Debug.Log (gameTypes.Length);
 		// Set playerNums ArrayList
 		foreach (int number in playerNumbers)
 			availablePlayerNums.Add(number);
@@ -30,7 +32,14 @@ public class MatchManager : MonoBehaviour {
 		GameObject.FindGameObjectWithTag ("Round").GetComponent<Text> ().text = "Round " + roundNumber;
 
 		// Select random GameType and set GameType text
-		int g = Random.Range (0, gameTypes.Length);
+		int g;
+		if (OnClickEvent.matchType == "") {
+			g = Random.Range (0, gameTypes.Length);
+		} else {
+			g = OnClickEvent.f;
+			Debug.Log(g);
+		}
+		Debug.Log (gameTypes.Length);
 		gameType = gameTypes [g];
 		GameObject.FindGameObjectWithTag ("GameType").GetComponent<Text> ().text = gameType;
 
@@ -39,13 +48,13 @@ public class MatchManager : MonoBehaviour {
 				teamDynamic = "FFA";
 		else if (gameType == "Team Deathmatch")
 				teamDynamic = "2v2";
-		else if (gameType == "King of the Hill") {
-				int randInt = Random.Range (0, 2);
-				teamDynamic = possibleTeamDynamics [randInt];
-		} else if (gameType == "Blowdown") {
-				teamDynamic = "FFA";	
-		}
+		else if (gameType == "King of the Hill") 
+				teamDynamic = "FFA";
+		else if (gameType == "Team KOTH") 
+				teamDynamic = "2v2";
 
+
+		Debug.Log(gameType);
 		// If team dynamic is free-for-all, display FFA text
 		if (teamDynamic == "FFA")
 						GameObject.FindGameObjectWithTag ("Teams").GetComponent<Text> ().text = "P1 vs P2 vs P3 vs P4";
@@ -53,6 +62,7 @@ public class MatchManager : MonoBehaviour {
 		else if (teamDynamic == "2v2") {
 				// Set up 2 vs 2 teams
 				// Pick 2 random player numbers for team 1
+				Debug.Log(teamDynamic);
 				while (team_1.Count < 2) {
 						int p = Random.Range (1, availablePlayerNums.Count);
 						team_1.Add (availablePlayerNums [p - 1]);
@@ -68,25 +78,10 @@ public class MatchManager : MonoBehaviour {
 
 				// Display Teams text
 				displayString = "P" + team1 [0] + " + P" + team1 [1] + "\nvs\nP" + team2 [0] + " + P" + team2 [1];
-				GameObject.FindGameObjectWithTag ("Teams").GetComponent<Text> ().text = displayString;
-		} else if (teamDynamic == "1v3") {
-				// Set up 1 vs 3 teams
-				// Pick 1 random player numbers for team 1
-				int p = Random.Range (1, availablePlayerNums.Count);
-				team_1.Add (availablePlayerNums [p - 1]);
-				availablePlayerNums.Remove (availablePlayerNums [p - 1]);
-				// Put the remaining 2 player numbers into team 2
-				foreach (int number in availablePlayerNums)
-					team_2.Add (number);
 				
-				// Turn team ArrayLists into arrays
-				team1 = team_1.ToArray ();
-				team2 = team_2.ToArray ();
-				
-				// Display Teams text
-				displayString = "P" + team1 [0] + "\nvs\nP" + team2 [0] + " + P" + team2 [1]  + " + P" + team2 [2];
+				Debug.Log(displayString);
 				GameObject.FindGameObjectWithTag ("Teams").GetComponent<Text> ().text = displayString;
-		}
+		} 
 
 	}
 	
@@ -94,7 +89,7 @@ public class MatchManager : MonoBehaviour {
 	void Update () {
 		timePassed += Time.deltaTime;
 		if (timePassed >= timeToNextRound)
-			Application.LoadLevel(Random.Range(2, Application.levelCount - 1));
+			Application.LoadLevel(3);
 	}
 	
 }
