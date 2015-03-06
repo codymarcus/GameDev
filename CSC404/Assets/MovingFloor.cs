e2a83222d338af4cf8a991c91be43ff922ea6541
Nicholas Fisher <avidnick@gmail.com>
2015-03-06 16:59:21 -0500

    1

--- CSC404/Assets/MovingFloor.cs
+++ CSC404/Assets/MovingFloor.cs
@@ -12,9 +12,11 @@ public class MovingFloor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		/*
 		GetComponent<Rigidbody>().velocity = new Vector3 (velocity, 0, 0);
 
 		if (transform.position.x >= 42)
 			transform.position = new Vector3(-42, transform.position.y);
+		*/
 	}
 }
