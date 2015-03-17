30d0b0c018331824b529d76af77d9d1942fb1395
Sida Chen <sidachen@SidadeMacBook-Pro.local>
2015-03-17 14:51:21 -0400

    start screen

---
+++ CSC404/Assets/StartManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartManager : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space"))
+			Application.LoadLevel ("Scene0");
+	}
+}
