7e8fcbdcf7ec287ac2cc8cce50914eeceabb3d06
Sida Chen <sidachen@SidadeMacBook-Pro.local>
2015-03-22 14:09:23 -0400

    add "go" to scene0

--- CSC404/Assets/ShowStartTime.cs
+++ CSC404/Assets/ShowStartTime.cs
@@ -8,6 +8,7 @@ public class ShowStartTime : MonoBehaviour {
 	public GameObject text1;
 	public GameObject text2;
 	public GameObject text3;
+	public GameObject text4;
 
 	// Update is called once per frame
 	void Update () {
@@ -17,15 +18,20 @@ public class ShowStartTime : MonoBehaviour {
 			text1.SetActive(true);
 			text2.SetActive(false);
 			text3.SetActive(false);
+			text4.SetActive (false);
 		}else if (gameStartTime < 2.0 && gameStartTime > 1.0) {
 			text2.SetActive(true);
 			text1.SetActive(false);
 		}else if (gameStartTime < 1.0 && gameStartTime > 0.0) {
 			text2.SetActive(false);
 			text3.SetActive(true);
-		}else if (gameStartTime < 0.0) {
+		}else if (gameStartTime < 0.0 && gameStartTime > -1.0) {
 			text3.SetActive(false);
+			text4.SetActive (true);
+		}else if(gameStartTime < -1.0) {
+			text4.SetActive(false);
 		}
+
 						
 	}
 }
