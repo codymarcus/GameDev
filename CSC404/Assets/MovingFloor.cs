f186c31fce08b4b9bc06227532bdb73389c946f1
codymarcus <codydavidmarcus@gmail.com>
2015-03-23 23:01:43 -0400

    Moving platform

--- CSC404/Assets/MovingFloor.cs
+++ CSC404/Assets/MovingFloor.cs
@@ -3,18 +3,44 @@ using System.Collections;
 
 public class MovingFloor : MonoBehaviour {
 
-	public float velocity = 5f;
+	public float xVelocity = 5f;
+	public float yVelocity = 5f;
+	public float xRange;
+	public float yRange;
+	float startX;
+	float startY;
+	bool xOut = false;
+	bool yOut = false;
+	float thisXVelocity;
+	float thisYVelocity;
 
 	// Use this for initialization
 	void Start () {
-
+		startX = transform.position.x;
+		startY = transform.position.y;
+		thisXVelocity = xVelocity/50;
+		thisYVelocity = yVelocity/50;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GetComponent<Rigidbody>().velocity = new Vector3 (velocity, 0, 0);
 
-		if (transform.position.x >= 42)
-			transform.position = new Vector3(-42, transform.position.y);
+		transform.position = new Vector3(transform.position.x + thisXVelocity, transform.position.y + thisYVelocity);
+
+		if (!xOut && (transform.position.x > startX + xRange || transform.position.x < startX))
+		{
+			thisXVelocity = -thisXVelocity;
+			xOut = true;
+		}
+		else
+			xOut = false;
+
+		if (!yOut && (transform.position.y > startY + yRange || transform.position.y < startY))
+		{
+			thisYVelocity = -thisYVelocity;
+			yOut = true;
+		}
+		else
+			yOut = false;
 	}
 }
