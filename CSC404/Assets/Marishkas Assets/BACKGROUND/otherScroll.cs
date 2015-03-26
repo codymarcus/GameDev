e43d67fe7a4deca59e853a82aed5fffdde294d75
marishkazachariah <mz12pl@student.ocadu.ca>
2015-03-25 21:33:12 -0400

    parallax scrolling

---
+++ CSC404/Assets/Marishkas Assets/BACKGROUND/otherScroll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class otherScroll : MonoBehaviour
+{
+		public float speed = 0;
+		public static otherScroll current;
+		private Material[] materials;
+		private Renderer rend;
+	
+		// Use this for initialization
+		void Start ()
+		{
+				rend = GetComponent<Renderer> ();
+				rend.enabled = true;
+				current = this;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+				Vector2 offset = new Vector2 (Time.time * speed, 0);
+				rend.material.mainTextureOffset = offset;
+		}
+}
