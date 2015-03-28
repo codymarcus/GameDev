bd3480b28e223ccfd089c775e3610c6bb9fba373
marishkazachariah <mz12pl@student.ocadu.ca>
2015-03-28 11:44:36 -0400

    day/night cycle implemented

---
+++ CSC404/Assets/Marishkas Assets/BACKGROUND/Moving.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Moving : MonoBehaviour {
+
+	public Transform movingSun;
+	public Transform position1;
+	public Transform position2;
+	public Vector3 newPosition;
+	public string currentState;
+	public float smooth;
+	public float resetTime;
+	// Use this for initialization
+	void Start () {
+		ChangeTarget ();
+	
+	}
+	
+	// Update is called once per frame
+	void fixedUpdate () {
+
+		movingSun.position = Vector3.Lerp (movingSun.position, newPosition, smooth = Time.deltaTime);
+	
+	}
+
+	void ChangeTarget () {
+		if(currentState == "Moving To Position 1") {
+			currentState = "Moving to Position 2";
+			newPosition = position2.position;
+		}
+		else if(currentState == "Moving To Position 2") {
+			currentState = "Moving to Position 1";
+			newPosition = position1.position;
+		}
+		else if(currentState == "") {
+			currentState = "Moving to Position 2";
+			newPosition = position2.position;
+		}
+
+		Invoke ("ChangeTarget", resetTime);
+	}
+}
---
+++ CSC404/Assets/Marishkas Assets/BACKGROUND/Transition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Transition : MonoBehaviour {
+float duration = 18.0f;
+public SpriteRenderer sRender;
+// Update is called once per frame
+Color HexToColor(string hex)
+{
+	byte r = byte.Parse(hex.Substring(0,2), System.Globalization.NumberStyles.HexNumber);
+	byte g = byte.Parse(hex.Substring(2,2), System.Globalization.NumberStyles.HexNumber);
+	byte b = byte.Parse(hex.Substring(4,2), System.Globalization.NumberStyles.HexNumber);
+	return new Color32(r,g,b, 255);
+}
+
+
+void Update () {
+	float lerp = Mathf.PingPong(Time.time, duration) / duration;
+		sRender.material.SetColor("_Color", Color.Lerp(HexToColor("c0e4eb"),HexToColor("424450"), lerp)); //bottom
+		sRender.material.SetColor("_Color2", Color.Lerp(HexToColor("c0e4eb"),HexToColor("424450"), lerp)); //top
+}
+}
