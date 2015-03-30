d488b7b5577405a46e700501a338534655d11e24
Kyle Cao <kyle.cao@softgravity.com>
2015-03-29 20:24:32 -0400

    implement witch

---
+++ CSC404/Assets/Witch.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Witch : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float fireratio = 1.0f;
+
+	public float movementSpeed = 0.1f;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (fireratio < 0) {
+			GameObject b = new GameObject ();
+			b = Instantiate (Resources.Load ("bulletGreen2"), transform.position, new Quaternion (180, 0, 0, 0)) as GameObject;
+			fireratio = 1.0f;
+		} else {
+			fireratio -= Time.deltaTime;
+		}
+		transform.position = new Vector3 (transform.position.x + movementSpeed*Time.deltaTime, transform.position.y, transform.position.z);
+		Destroy(this.gameObject, 20.0f);
+	}
+}
---
+++ CSC404/Assets/WitchManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WitchManager : MonoBehaviour {
+
+	// Use this for initialization
+
+	public float spawnratio = 20.0f;
+
+	public GameObject witch;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject w;
+		if (spawnratio < 0) {
+			w = Instantiate (witch, new Vector3 (transform.position.x, transform.position.y),
+                new Quaternion (0, 180, 0, 0)) as GameObject;
+			spawnratio = 20.0f;
+		} else {
+			spawnratio -= Time.deltaTime;
+		}
+	}
+}
