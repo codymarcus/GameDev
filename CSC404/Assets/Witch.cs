23d247094830551ef81078304c754847b1ea9c82
Nicholas-Fisher <avidnick@gmail.com>
2015-04-01 19:12:25 -0400

    day night and watch rotate

--- CSC404/Assets/Witch.cs
+++ CSC404/Assets/Witch.cs
@@ -6,13 +6,18 @@ public class Witch : MonoBehaviour {
 	// Use this for initialization
 
 	public float fireratio = 1.0f;
-	public float initial_y;
-	public float amplitude = 1.0f;
-	public float speed = 1.0f;
-	public float movementSpeed = 3.0f;
+	 float initial_y;
+	 float initial_z;
+	 float amplitude = 1.0f;
+	 float speed = 1.0f;
+	 float amplitude2 = 4.0f;
+	 float speed2 = 5.0f;
+	 float movementSpeed = 3.0f;
 
 	void Start () {
 		initial_y = transform.position.y;
+		//transform.rotation = Quaternion.Euler(-43.1f,180,0);
+		initial_z = transform.rotation.z;
 		Destroy(this.gameObject, 20.0f);
 	}
 	
@@ -29,9 +34,11 @@ public class Witch : MonoBehaviour {
 		} else {
 			fireratio -= Time.deltaTime;
 		}
+		float new_z = initial_z + amplitude2 * Mathf.Sin (speed2 * Time.time);
 		float new_y = initial_y + amplitude * Mathf.Sin (speed * Time.time);
 		transform.position = new Vector3(transform.position.x + movementSpeed*Time.deltaTime, new_y, transform.position.z);
-
+		transform.Rotate (new_z, 0, 0);
+		//print (transform.rotation.eulerAngles);
 		//transform.position = new Vector3 (transform.position.x + movementSpeed*Time.deltaTime, transform.position.y, transform.position.z);
 		//Destroy(this.gameObject, 20.0f);
 	}
